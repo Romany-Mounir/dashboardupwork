e9b3cfd6e89ad9a47515be8f6858076f98ad9b4a
alaa-refat <alaarefatmohmmed@gmail.com>
2021-04-18 14:19:16 +0200

    dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar/sidebar.component';
+import { SigninComponent } from './pages/external/signin/signin/signin.component';
+import { SignupComponent } from './pages/external/signup/signup/signup.component';
+import { ResetPaswordComponent } from './pages/external/reset-pasword/reset-pasword.component';
+import { ErrorPageComponent } from './pages/external/error-page/error-page.component';
+import { OverviewComponent } from './pages/overview/overview.component';
+import { DocsComponent } from './pages/docs/docs.component';
+import { OrdersComponent } from './pages/orders/orders.component';
+import { ChartsComponent } from './pages/charts/charts.component';
+import { HelpComponent } from './pages/help/help.component';
+import { NotificationsComponent } from './pages/notifications/notifications.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { AccountComponent } from './pages/account/account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    SigninComponent,
+    SignupComponent,
+    ResetPaswordComponent,
+    ErrorPageComponent,
+    OverviewComponent,
+    DocsComponent,
+    OrdersComponent,
+    ChartsComponent,
+    HelpComponent,
+    NotificationsComponent,
+    SettingsComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
